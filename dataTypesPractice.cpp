a5ea76f79496471a606a515b38aca26bb78bf797
Jason Palacio <meteor.man1989@gmail.com>
2024-05-09 22:01:30 -0700

    practice with c++ data types and byte size included notes to track

---
+++ dataTypesPractice.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+
+
+	char test = '?';
+	char test2 = 'A';
+	bool chooseSwitch;
+
+	string myMessage = "Hello there World!";
+
+	cout << "This is the size of the following: " << sizeof(test) << endl;
+	cout << "This is the size of the following: " << sizeof(test2) << endl;
+	cout << "This is the size of the following: " << sizeof(chooseSwitch) << endl;
+	cout << "This is the size of the following: " << sizeof(test2) << endl;
+	cout << "This is the size of the following: " << sizeof(myMessage) << endl;
+}
---
+++ notes.md
@@ -0,0 +1,22 @@
+# Number systems in C++ 
+
+0b -- prefix signifies binary
+F or f -- suffix signifies hexadecimal numbers
+
+
+# Variables naming and sizes
+
+char = 1 byte
+boolean = 1 byte
+std::string = 24 bytes
+int = 4 bytes
+float = 4 bytes
+double = 8 bytes
+
+int will keep to integers and chop off decimal numbers
+
+# different initializer types
+
+{} for int data types compiler will spit out an error about the data type whereas a braced initializer () will just run the code and chop off information from the initializer.
+
+#
